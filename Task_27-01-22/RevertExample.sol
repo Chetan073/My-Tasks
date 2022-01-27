44a78b276be1b10a55af7ef66ef85051375a6e3b
Chetan073 <98375536+Chetan073@users.noreply.github.com>
2022-01-27 19:12:32 +0530

    Create RevertExample.sol

---
+++ Task_27-01-22/RevertExample.sol
@@ -0,0 +1,22 @@
+pragma solidity ^0.5.0;
+  
+// Creating a contract
+contract revertStatement {
+    
+    // Defining a function
+    // to check condition
+    function checkOverflow(
+      uint _num1, uint _num2) public view returns(
+      string memory, uint) {
+        uint sum = _num1 + _num2;
+         if(sum < 0 || sum > 255){
+             revert(" Overflow Exist");
+         }
+         else{
+             return ("No Overflow", sum);
+         }
+           
+    }
+      
+}
+      
